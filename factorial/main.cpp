774d083983ef1bb7ae20e1c099d44152fd62d7c7
Martin Di Paola <martinp.dipaola@gmail.com>
2022-07-24 18:28:06 -0300

    refactor: read_num separado de main
    El parsing del numero era demasiado codigo en la funcion
    main. Movido a una funcion separada (read_num).

--- factorial/main.cpp
+++ factorial/main.cpp
@@ -7,6 +7,34 @@ void not_supported_print() {
     std::cerr << "Not supported.\n";
 }
 
+int read_num(const char *str_num) {
+    int num;
+    size_t idx;
+    try {
+        num = std::stoi(str_num, &idx);
+    } catch (...) {
+        // Si el string parseado es invalido o
+        // esta fuera del rango de int, stoi() lanzara
+        // una excepcion.
+        // Para nosotros es un valor no soportado.
+        return -1;
+    }
+
+    // Factorial de numeros negativos no esta soportado
+    if (num < 0) {
+        return -1;
+    }
+
+    // El string tiene mas caracteres que los numericos
+    // Por ejemplo "0.25" es parseado por stoi como "0"
+    // Esto no esta soportado por factorial.
+    if (strlen(str_num) != idx) {
+        return -1;
+    }
+
+    return num;
+}
+
 int factorial(int num) {
     if (num == 0)
         return 1;
@@ -15,29 +43,8 @@ int factorial(int num) {
 }
 
 int main(int argc, char *argv[]) {
-    int num;
-    size_t idx;
-    try {
-        num = std::stoi(argv[1], &idx);
-    } catch (...) {
-        // Si el string parseado es invalido o
-        // esta fuera del rango de int, stoi() lanzara
-        // una excepcion.
-        // Para nosotros es un valor no soportado.
-        not_supported_print();
-        return -1;
-    }
-
-    // Factorial de numeros negativos no esta soportado
+    int num = read_num(argv[1]);
     if (num < 0) {
-        not_supported_print();
-        return -1;
-    }
-
-    // El string tiene mas caracteres que los numericos
-    // Por ejemplo "0.25" es parseado por stoi como "0"
-    // Esto no esta soportado por factorial.
-    if (strlen(argv[1]) != idx) {
         not_supported_print();
         return -1;
     }
