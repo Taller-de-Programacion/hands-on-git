9c6a0fb316b0d0e5a0c508613837e96cd7c8cf21
Martin Di Paola <martinp.dipaola@gmail.com>
2022-07-24 18:20:43 -0300

    arreglo bug, vamos que los tests pasan!!

--- factorial/main.cpp
+++ factorial/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 #include <string>
 
 int factorial(int num) {
@@ -10,13 +11,28 @@ int factorial(int num) {
 
 int main(int argc, char *argv[]) {
     int num;
+    size_t idx;
     try {
-        num = std::stoi(argv[1]);
+        num = std::stoi(argv[1], &idx);
     } catch (...) {
         // Si el string parseado es invalido o
         // esta fuera del rango de int, stoi() lanzara
         // una excepcion.
         // Para nosotros es un valor no soportado.
+        std::cerr << "Not supported.\n";
+        return -1;
+    }
+
+    // Factorial de numeros negativos no esta soportado
+    if (num < 0) {
+        std::cerr << "Not supported.\n";
+        return -1;
+    }
+
+    // El string tiene mas caracteres que los numericos
+    // Por ejemplo "0.25" es parseado por stoi como "0"
+    // Esto no esta soportado por factorial.
+    if (strlen(argv[1]) != idx) {
         std::cerr << "Not supported.\n";
         return -1;
     }
