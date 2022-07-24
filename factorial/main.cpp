c8ed07beb09eddddf3536b80e141239bb92ca703
Martin Di Paola <martinp.dipaola@gmail.com>
2022-07-24 18:22:37 -0300

    not_supported_print

--- factorial/main.cpp
+++ factorial/main.cpp
@@ -2,6 +2,11 @@
 #include <cstring>
 #include <string>
 
+inline
+void not_supported_print() {
+    std::cerr << "Not supported.\n";
+}
+
 int factorial(int num) {
     if (num == 0)
         return 1;
@@ -19,13 +24,13 @@ int main(int argc, char *argv[]) {
         // esta fuera del rango de int, stoi() lanzara
         // una excepcion.
         // Para nosotros es un valor no soportado.
-        std::cerr << "Not supported.\n";
+        not_supported_print();
         return -1;
     }
 
     // Factorial de numeros negativos no esta soportado
     if (num < 0) {
-        std::cerr << "Not supported.\n";
+        not_supported_print();
         return -1;
     }
 
@@ -33,7 +38,7 @@ int main(int argc, char *argv[]) {
     // Por ejemplo "0.25" es parseado por stoi como "0"
     // Esto no esta soportado por factorial.
     if (strlen(argv[1]) != idx) {
-        std::cerr << "Not supported.\n";
+        not_supported_print();
         return -1;
     }
 
